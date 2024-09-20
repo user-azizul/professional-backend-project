efc7dc05cc4654e793be9fde0a4428a2f2ee5f05
user-azizul <azizul.contact.me@gmail.com>
2024-09-20 22:52:56 +0300

    cloudinary and multer intigretion

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //upload file on cloudinary server
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been uploaded successfully
+    console.log("file is uploader on cloudinary", response.url);
+    return response;
+  } catch (err) {
+    fs.unlinkSync(localFilePath); // remove the localy saved temporary file as the upload operation got failed
+    console.log(err, "from cloudinary file");
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
